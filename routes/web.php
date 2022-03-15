8fc9ba59f8c93011fcd1433fc71dc934b0dadd68
tpjigar <tp.jigar@gmail.com>
2022-03-15 19:44:39 +0530

    remove alpine and package install vue3 and vue-loader

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::group(['middleware' => ['auth']], function() {
+
+});
+
 require __DIR__.'/auth.php';
